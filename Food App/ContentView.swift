90ae41f8f89221ce86c5ee8e977a5cfc194c5b38
Dmitriy Borisov <dm.borisov1982@gmail.com>
2021-01-25 12:11:48 +0300

    Initial Commit

---
+++ Food App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Food App
+//
+//  Created by Dmitriy Borisov on 25.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Food App/Food_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_AppApp.swift
+//  Food App
+//
+//  Created by Dmitriy Borisov on 25.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct Food_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
